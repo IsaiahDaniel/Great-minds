61b67498e3ffa0b41089de5ecbf5b4466080dece
Isaiah Daniel <isaiahdaniel203@gmail.com>
2022-09-07 09:11:41 +0100

    instagram api

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 // import Button from './Button';
-import { BsInstagram } from "react-icons/bs";
+import { BsInstagram } from 'react-icons/bs'
 
 const Footer = () => {
-    return (
-        <>
-            <FooterWrapper>
-                <div className="mt-5 text-center text-white">
-                    <div className="container grid-3">
+  return (
+    <>
+      <FooterWrapper>
+        <div className="mt-5 text-center text-white">
+          <div className="container grid-3">
+            <div>
+              <h3>Address</h3>
+              <p>3721 Single Street Quincy, MA 02169</p>
+            </div>
 
-                        <div>
-                            <h3>Address</h3>
-                            <p>
-                                3721 Single Street
-                                Quincy, MA 02169
-                            </p>
-                        </div>
-
-                        {/* <div>
+            {/* <div>
                             <h3>FAQS</h3>
                             <ul>
                                 <li className="mb-2">3721 Single Street</li>
@@ -27,48 +23,48 @@ const Footer = () => {
                             </ul>
                         </div> */}
 
-                        <div>
-                            <h3>Contacts</h3>
-                            <p>
-                                +234567392021
-                            </p>
-                            <p>
-                                info@brilliantminds.com
-                            </p>
-                        </div>
+            <div>
+              <h3>Contacts</h3>
+              <p>+234567392021</p>
+              <p>info@brilliantminds.com</p>
+            </div>
 
-                        <a href='https://www.instagram.com/greatmindsliteracyreaders/' target="_blank" style={{ textDecoration: "none", color: "#fff" }}  rel="noreferrer">
-                            <h3>Socials</h3>
-                            <br />
-                            <BsInstagram size="20" />
-                        </a>
+            <a
+              href="https://www.instagram.com/greatmindsliteracyreaders/"
+              target="_blank"
+              style={{ textDecoration: 'none', color: '#fff' }}
+              rel="noreferrer"
+            >
+              <h3>Socials</h3>
+              <br />
+              <BsInstagram size="20" />
+            </a>
 
-                        {/* <div>
+            {/* <div>
                     <h3>Subscribe to our newsletter</h3>
                     <form>
                         <Input type="text" className="form-control mb-4" />
                         <Button text="submit"  />
                     </form>
                 </div> */}
-                    </div>
-                </div>
-            </FooterWrapper>
-            <div className="footer-bottom text-center text-white bg-dark p-2">
-                <h3>GreatMinds Literacy Reader &copy; 2021</h3>
-            </div>
-
-        </>
-    );
+          </div>
+        </div>
+      </FooterWrapper>
+      <div className="footer-bottom text-center text-white bg-dark p-2">
+        <h3>GreatMinds Literacy Reader &copy; 2021</h3>
+      </div>
+    </>
+  )
 }
 
 const FooterWrapper = styled.footer`
-    background-color:  ${({ theme }) => theme.dark};
-    padding: 20px;
-`;
+  background-color: ${({ theme }) => theme.dark};
+  padding: 20px;
+`
 
 // const Input = styled.input`
 //     padding: 10px 20px;
 //     width: 100%;
 // `;
 
-export default Footer;
+export default Footer
--- src/pages/Insights.js
+++ src/pages/Insights.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import cardImgOne from '../assets/instagram-1.jpg'
 import cardImgTwo from '../assets/instagram-2.jpg'
-import cardImgThree from '../assets/card-img-3.jpg'
+import cardImgThree from '../assets/instagram-3.jpg'
+import cardImgFour from '../assets/instagram-4.jpg'
+import cardImgFive from '../assets/instagram-5.jpg'
 import styled from 'styled-components'
 
 import './insights.css';
@@ -15,41 +17,42 @@ const Insights = () => {
         <h1 className="text-center">Insights</h1>
         <p className="text-center mb-4">Take a Peak at what we are trying to Accomplish</p>
       </div>
-        <div className="grid-3 flex-column-sm text-center">
-          <a href="https://www.instagram.com/p/Ch-Ii3Vj1l7/" target="_blank" rel="noreferrer">
-            <img src={cardImgOne} height="500" alt="instagram-one" className="mr-3" data-aos="fade-up" />
-            <Paragraph>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, perspiciatis.
-            </Paragraph>
-          </a>
-          <a href='https://www.instagram.com/p/ChxkCd8jG6c/' target="_blank" rel="noreferrer">
-            <img src={cardImgTwo} height="500" alt="instagram-one" className="mr-3" data-aos="fade-up" /> 
-            <Paragraph>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, perspiciatis.
-            </Paragraph>
-          </a>
-        </div>
 
-        <div className="grid-3 flex-column-sm text-center">
-          <div>
-            <img src={cardImgOne} height="500" className="mr-3" alt="instagram-one" data-aos="fade-up" />
-            <Paragraph>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, perspiciatis.
-            </Paragraph>
-          </div>
-          <div>
-            <img src={cardImgThree} height="500" className="mr-3" alt="instagram-two" data-aos="fade-up" />
-            <Paragraph>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, perspiciatis.
-            </Paragraph>
-          </div>
-          <div>
-            <img src={cardImgTwo} height="500" className="mr-3" alt="instagram-three" data-aos="fade-up" /> 
-            <Paragraph>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, perspiciatis.
-            </Paragraph>
-          </div>
-        </div>
+      <div className="grid-3 flex-column-sm text-center">
+        <a href="https://www.instagram.com/p/Ch-Ii3Vj1l7/" target="_blank" rel="noreferrer">
+          <img src={cardImgOne} height="500" alt="instagram-one" className="mr-3" data-aos="fade-up" />
+          <Paragraph>
+            Month of taking over
+          </Paragraph>
+        </a>
+        <a href="https://www.instagram.com/p/ChXBGhVj9XG/" target="_blank" rel="noreferrer">
+          <img src={cardImgThree} height="500" className="mr-3" alt="instagram-two" data-aos="fade-up" />
+          <Paragraph>
+          Self defense:the act of defending one's self when physically attacked, as by countering blows or overcoming an assailant.
+          </Paragraph>
+        </a>
+        <a href="https://www.instagram.com/p/ChxkCd8jG6c/" target="_blank" rel="noreferrer">
+          <img src={cardImgTwo} height="500" className="mr-3" alt="instagram-three" data-aos="fade-up" /> 
+          <Paragraph>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, perspiciatis.
+          </Paragraph>
+        </a>
+      </div>
+
+      <div className="grid-3 flex-column-sm text-center">
+        <a href="https://www.instagram.com/p/Chbj7doDR4B/" target="_blank" rel="noreferrer">
+          <img src={cardImgFour} height="500" className="mr-3" alt="instagram-one" data-aos="fade-up" />
+          <Paragraph>
+            Did you know that about 18.5 million children which about 60% of whom are girls are presently out of school- UN children fund
+          </Paragraph>
+        </a>
+        <a href="https://www.instagram.com/p/ChmseWSDI0i/" target="_blank" rel="noreferrer">
+          <img src={cardImgFive} height="500" className="mr-3" alt="instagram-three" data-aos="fade-up" /> 
+          <Paragraph>
+          Do you have a daughter, sister or you know someone within this age range who needs to be here?
+          </Paragraph>
+        </a>
+      </div>
     </Wrapper>
   )
 }
